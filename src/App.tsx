c4468dc9e21c6c45357c401ee9216e93f0db598d
PraneethGunas <praneethgunas404@gmail.com>
2024-06-04 12:46:31 +0530

    chore: Update babel.config.js and package.json for react-native-paper integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,15 @@
 
 import React from 'react';
 import Navigator from './navigation/Navigator';
+import { PaperProvider } from 'react-native-paper';
+import { theme } from './theme';
 
 function App() {
-  return <Navigator />;
+  return (
+    <PaperProvider theme={theme}>
+      <Navigator />
+    </PaperProvider>
+  );
 }
 
 export default App;
