263f364f0ddfeb55d85fdbc5d68f928ee7d0d15c
akshay8551 <akshaychavan8551@gmail.com>
2024-06-04 16:15:00 +0530

    Designed card image surface

---
+++ src/components/CardImageSurface.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Avatar } from 'react-native-paper';
+
+const CardImageSurface = () => (
+  <Avatar.Image size={70} source={require('../assets/avatar.png')} />
+);
+export default CardImageSurface;
---
+++ src/components/CardImageSurfaceSmall.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Avatar } from 'react-native-paper';
+
+const CardImageSurfaceSmall = () => (
+  <Avatar.Image size={50} source={require('../assets/avatar.png')} />
+);
+export default CardImageSurfaceSmall;
