a9cc7a026eb06b3d468dcbfd5440a1658c75a4ca
akshay8551 <akshaychavan8551@gmail.com>
2024-06-05 12:43:32 +0530

    import and used fonts

--- src/components/PrimaryCTA.tsx
+++ src/components/PrimaryCTA.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react';
+import { StyleSheet } from 'react-native';
 import { Button, useTheme } from 'react-native-paper';
+
+import Fonts from '../constants/Fonts';
 
 function PrimaryCTA(props) {
   const theme = useTheme();
   return (
     <Button
       mode="contained"
+      uppercase={false}
+      labelStyle={styles.ctaLabelStyle}
+      style={styles.ctaContainerStyle}
       buttonColor={theme.colors.primaryCTA}
-      onPress={() => console.log('Pressed')}>
+      onPress={props.onPress}>
       {props.title}
     </Button>
   );
 }
-
+const styles = StyleSheet.create({
+  ctaContainerStyle: {
+    borderRadius: 10,
+  },
+  ctaLabelStyle: {
+    fontSize: 13,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+});
 export default PrimaryCTA;
