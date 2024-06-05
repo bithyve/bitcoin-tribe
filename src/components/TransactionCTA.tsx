a9f5045704762382bb201030b0fc63bd0204c681
akshay8551 <akshaychavan8551@gmail.com>
2024-06-05 13:36:48 +0530

    TransactionCTA - designed new component

---
+++ src/components/TransactionCTA.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+
+import Fonts from '../constants/Fonts';
+
+function TransactionCTA(props) {
+  return (
+    <Button
+      testID="btn_TransactionCTA"
+      icon={() => props.icon}
+      mode="contained"
+      uppercase={false}
+      labelStyle={styles.ctaLabelStyle}
+      style={styles.ctaContainerStyle}
+      buttonColor={props.buttonColor}
+      onPress={props.onPress}>
+      {props.title}
+    </Button>
+  );
+}
+const styles = StyleSheet.create({
+  ctaContainerStyle: {
+    borderRadius: 25,
+  },
+  ctaLabelStyle: {
+    fontSize: 13,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+});
+export default TransactionCTA;
