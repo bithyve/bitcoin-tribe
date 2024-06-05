18940f7ae7306876601067e86ab5c247227ca57f
akshay8551 <akshaychavan8551@gmail.com>
2024-06-05 12:41:33 +0530

    Added new colors

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -2,5 +2,7 @@ const Colors = {
   ChineseOrange: 'rgba(249, 118, 67,1)',
   Eucalyptus: 'rgba(59, 204, 160, 1)',
   SelectiveYellow: 'rgba(255, 186, 0, 1)',
+  Jet: 'rgba(54, 54, 54,1)',
+  ChineseWhite: 'rgba(224, 224, 224, 1)',
 };
 export default Colors;
