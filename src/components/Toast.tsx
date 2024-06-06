988100e33d0245b7d06c66188db44306fd4eb8d4
akshay8551 <akshaychavan8551@gmail.com>
2024-06-06 17:04:38 +0530

    Designed Toast and remove snackbar

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,33 +1,47 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { Snackbar, useTheme } from 'react-native-paper';
+import { StyleSheet, View, Text } from 'react-native';
+import Toast from 'react-native-root-toast';
 
-type ToastProps = {
-  visible: boolean;
-  onDismissSnackBar?: any;
-  message?: string;
-};
-
-const Toast = (props: ToastProps) => {
-  const theme = useTheme();
-  const styles = getStyles(theme);
-  return (
-    <Snackbar
-      testID="toast_Snackbar"
-      visible={props.visible}
-      onDismiss={props.onDismissSnackBar}
-      elevation={5}
-      //   duration={2000}
-      style={styles.container}>
-      {props.message}
-    </Snackbar>
+import Colors from '../theme/Colors';
+import CheckIcon from '../assets/images/icon_check.svg';
+import CommonStyles from '../common/styles/CommonStyles';
+// Need to work
+export default (message, icon = false, error = false) => {
+  return Toast.show(
+    <View style={styles.container}>
+      {icon && <CheckIcon />}
+      <Text style={[CommonStyles.toastMessage, styles.toastMessageStyle]}>
+        {message && message.length > 100
+          ? `${message.substring(0, 100)}...`
+          : message}
+      </Text>
+    </View>,
+    {
+      duration: Toast.durations.SHORT,
+      position: Toast.positions.BOTTOM,
+      shadow: true,
+      animation: true,
+      hideOnPress: true,
+      delay: 0,
+      opacity: 1,
+      backgroundColor: Colors.ChineseOrange,
+      textColor: Colors.RaisinBlack,
+    },
   );
 };
-const getStyles = theme =>
-  StyleSheet.create({
-    container: {
-      backgroundColor: theme.colors.primaryCTA,
-      borderRadius: 10,
-    },
-  });
-export default Toast;
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: 300,
+    minHeight: 60,
+    borderRadius: 50,
+  },
+  toastMessageStyle: {
+    color: Colors.RaisinBlack,
+    paddingLeft: 10,
+    flex: 1,
+    flexWrap: 'wrap',
+  },
+});
