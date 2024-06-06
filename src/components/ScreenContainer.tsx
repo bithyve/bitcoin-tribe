7943da03bf4f6e36190ce7aacc6819d9bf77abd2
akshay8551 <akshaychavan8551@gmail.com>
2024-06-06 11:11:23 +0530

    Added imagebackgound common container

---
+++ src/components/ScreenContainer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+
+const ScreenContainer = props => {
+  return (
+    <View style={{ ...styles.container, ...props.style }}>
+      <ImageBackground
+        source={require('../assets/images/image_background.png')}
+        resizeMode="cover"
+        style={styles.image}>
+        {props.children}
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default ScreenContainer;
