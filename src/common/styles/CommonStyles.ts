99c410d01edc2a0be8b28e549f5a498418515643
akshay8551 <akshaychavan8551@gmail.com>
2024-06-06 11:00:22 +0530

    Created/Added common styles

---
+++ src/common/styles/CommonStyles.ts
@@ -0,0 +1,74 @@
+import { StyleSheet } from 'react-native';
+
+import Fonts from '../../constants/Fonts';
+
+export default StyleSheet.create({
+  heading1: {
+    fontSize: 18,
+    fontFamily: Fonts.PoppinsMedium,
+  },
+  heading2: {
+    fontSize: 16,
+    fontFamily: Fonts.PoppinsMedium,
+  },
+  pageTitle: {
+    fontSize: 24,
+    fontFamily: Fonts.PoppinsMedium,
+  },
+  subTitle: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsRegular,
+  },
+  body1: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsRegular,
+  },
+  body2: {
+    fontSize: 12,
+    fontFamily: Fonts.PoppinsRegular,
+  },
+  body4: {
+    fontSize: 16,
+    fontFamily: Fonts.PoppinsLight,
+  },
+  body5: {
+    fontSize: 12,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+  body6: {
+    fontSize: 18,
+    fontFamily: Fonts.PoppinsBold,
+  },
+  body7: {
+    fontSize: 22,
+    fontFamily: Fonts.PoppinsBold,
+  },
+  placeholder: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsBold,
+  },
+  toastMessage: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+  textFieldLabel: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsMedium,
+  },
+  bottomNavigation: {
+    fontSize: 11,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+  primaryCTATitle: {
+    fontSize: 13,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+  secondaryCTATitle: {
+    fontSize: 13,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+  transactionCTATitle: {
+    fontSize: 14,
+    fontFamily: Fonts.PoppinsSemiBold,
+  },
+});
