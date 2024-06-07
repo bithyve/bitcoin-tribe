ec6d6164fc278f4055ec8b239a07fe093a889bbb
akshay8551 <akshaychavan8551@gmail.com>
2024-06-07 14:49:07 +0530

    Rename file name

--- src/components/AddNewTile.tsx
+++ src/components/AddNewTile.tsx
@@ -3,12 +3,12 @@ import { StyleSheet, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { hp, wp } from '../constants/responsive';
 
-type AddNewTileSurfaceProps = {
+type AddNewTileProps = {
   icon?: any;
   title?: string;
 };
 
-const AddNewTileSurface = (props: AddNewTileSurfaceProps) => {
+const AddNewTile = (props: AddNewTileProps) => {
   const theme = useTheme();
   const styles = getStyles(theme);
   return <View style={styles.container}></View>;
@@ -19,7 +19,7 @@ const getStyles = theme =>
       height: hp(150),
       width: wp(160),
       borderRadius: 10,
-      backgroundColor: theme.colors.textColor,
+      backgroundColor: theme.colors.cardBackground,
     },
   });
-export default AddNewTileSurface;
+export default AddNewTile;
