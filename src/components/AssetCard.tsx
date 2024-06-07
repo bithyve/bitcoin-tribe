afe6fc87760171866145da835eed1dcd0fb4f2a1
akshay8551 <akshaychavan8551@gmail.com>
2024-06-07 14:54:42 +0530

    Rename file name

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -4,13 +4,13 @@ import { useTheme } from 'react-native-paper';
 
 import { wp, hp } from '../constants/responsive';
 
-type TileSurfaceProps = {
+type AssetCardProps = {
   icon?: any;
   title?: string;
   details?: string;
 };
 
-const TileSurface = (props: TileSurfaceProps) => {
+const AssetCard = (props: AssetCardProps) => {
   const theme = useTheme();
   const styles = getStyles(theme);
   return <View style={styles.container}></View>;
@@ -20,8 +20,8 @@ const getStyles = theme =>
     container: {
       height: hp(205),
       width: wp(160),
-      borderRadius: 10,
-      backgroundColor: theme.colors.textColor,
+      borderRadius: 15,
+      backgroundColor: theme.colors.cardBackground,
     },
   });
-export default TileSurface;
+export default AssetCard;
