43ee6f583ae8daab4f1ec927b39aaf0aa2cf7c6d
akshay8551 <akshaychavan8551@gmail.com>
2024-06-07 07:13:42 +0530

    Replace paper's button by rn components

--- src/components/PrimaryCTA.tsx
+++ src/components/PrimaryCTA.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Button, useTheme } from 'react-native-paper';
 import CommonStyles from '../common/styles/CommonStyles';
+import { hp, wp } from '../constants/responsive';
 
 type PrimaryCTAProps = {
   onPress: any;
@@ -10,21 +11,40 @@ type PrimaryCTAProps = {
 
 function PrimaryCTA(props: PrimaryCTAProps) {
   const theme = useTheme();
+  const styles = getStyles(theme);
   return (
-    <Button
-      mode="contained"
-      uppercase={false}
-      labelStyle={CommonStyles.primaryCTATitle}
+    <TouchableOpacity
       style={styles.ctaContainerStyle}
-      buttonColor={theme.colors.primaryCTA}
-      onPress={props.onPress}>
-      {props.title}
-    </Button>
+      onPress={props.onPress}
+      testID="btn_primary">
+      <Text
+        style={[
+          CommonStyles.primaryCTATitle,
+          { color: theme.colors.textColor },
+        ]}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+    // <Button
+    //   mode="contained"
+    //   uppercase={false}
+    //   labelStyle={CommonStyles.primaryCTATitle}
+    //   style={styles.ctaContainerStyle}
+    //   buttonColor={theme.colors.primaryCTA}
+    //   onPress={props.onPress}>
+    //   {props.title}
+    // </Button>
   );
 }
-const styles = StyleSheet.create({
-  ctaContainerStyle: {
-    borderRadius: 10,
-  },
-});
+const getStyles = theme =>
+  StyleSheet.create({
+    ctaContainerStyle: {
+      borderRadius: 8,
+      height: hp(50),
+      minWidth: wp(150),
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: theme.colors.primaryCTA,
+    },
+  });
 export default PrimaryCTA;
