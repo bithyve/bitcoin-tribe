113e4d133ccb1cdfd1c13c1b4d70dc95f9301377
akshay8551 <akshaychavan8551@gmail.com>
2024-06-10 12:10:35 +0530

    Added profile setup and splash screen

---
+++ src/screens/onBoarding/ProfileSetup.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import ScreenContainer from '../../components/ScreenContainer';
+
+function ProfileSetup() {
+  return (
+    <ScreenContainer>
+      <Text>WalletSetupOption!</Text>
+    </ScreenContainer>
+  );
+}
+export default ProfileSetup;
