248b713da2d4c3c094c760c84713d4bd6fd498fa
akshay8551 <akshaychavan8551@gmail.com>
2024-06-10 12:09:43 +0530

    Designed option card component for walletSeupOption screen

---
+++ src/screens/onBoarding/components/OptionCard.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
+
+import AppText from '../../../components/AppText';
+import IconArrow from '../../../assets/images/icon_arrowr2.svg';
+import { hp } from '../../../constants/responsive';
+
+type OptionCardProps = {
+  icon?: any;
+  title: string;
+  subTitle: string;
+  style?: any;
+};
+
+function OptionCard(props: OptionCardProps) {
+  const { icon, title, subTitle, style } = props;
+  const theme = useTheme();
+  const styles = getStyles(theme);
+  return (
+    <View style={[styles.container, style]}>
+      <View>{icon}</View>
+      <View style={styles.detailsWrapper}>
+        <View style={styles.contentWrapper}>
+          <AppText style={styles.menuCardTitle}>{title}</AppText>
+          <AppText style={styles.menuCardSubTitle}>{subTitle}</AppText>
+        </View>
+        <View style={styles.iconWrapper}>
+          <IconArrow />
+        </View>
+      </View>
+    </View>
+  );
+}
+const getStyles = theme =>
+  StyleSheet.create({
+    container: {
+      width: '100%',
+      padding: hp(15),
+      marginVertical: hp(10),
+      borderRadius: 10,
+      backgroundColor: theme.colors.cardBackground,
+    },
+    detailsWrapper: {
+      flexDirection: 'row',
+      width: '100%',
+    },
+    contentWrapper: {
+      width: '90%',
+      marginTop: hp(10),
+    },
+    menuCardTitle: {
+      color: theme.colors.body,
+    },
+    menuCardSubTitle: {
+      color: theme.colors.body,
+    },
+    iconWrapper: {
+      width: '10%',
+      justifyContent: 'center',
+    },
+  });
+export default OptionCard;
