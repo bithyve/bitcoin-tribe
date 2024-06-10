c1cccdcdc3c16890939eaad39a2b0db1440120ae
akshay8551 <akshaychavan8551@gmail.com>
2024-06-10 15:34:42 +0530

    Added secondary button and done props destruction

--- src/components/PrimaryCTA.tsx
+++ src/components/PrimaryCTA.tsx
@@ -1,33 +1,68 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, useTheme } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Button, TouchableRipple, useTheme } from 'react-native-paper';
+
 import CommonStyles from '../common/styles/CommonStyles';
 import { hp, wp } from '../constants/responsive';
+import AppText from './AppText';
 
 type PrimaryCTAProps = {
-  onPress: any;
-  title: string;
+  primaryOnPress: any;
+  secondaryOnPress?: any;
+  primaryTitle: string;
+  width?: any;
+  style?: any;
+  secondaryTitle?: string;
 };
 
 function PrimaryCTA(props: PrimaryCTAProps) {
+  const {
+    primaryOnPress,
+    primaryTitle,
+    width,
+    secondaryTitle,
+    secondaryOnPress,
+  } = props;
   const theme = useTheme();
-  const styles = getStyles(theme);
+  const styles = getStyles(theme, width);
   return (
-    <Button
-      mode="contained"
-      uppercase={false}
-      labelStyle={[CommonStyles.primaryCTATitle, styles.labelStyle]}
-      style={styles.ctaContainerStyle}
-      buttonColor={theme.colors.primaryCTA}
-      onPress={props.onPress}>
-      {props.title}
-    </Button>
+    <View style={styles.container}>
+      {secondaryTitle && (
+        <TouchableRipple onPress={secondaryOnPress}>
+          <AppText
+            variant="secondaryCTATitle"
+            style={styles.seconadryTitleStyle}>
+            {secondaryTitle}
+          </AppText>
+        </TouchableRipple>
+      )}
+      <Button
+        mode="contained"
+        uppercase={false}
+        labelStyle={[CommonStyles.primaryCTATitle, styles.labelStyle]}
+        style={styles.ctaContainerStyle}
+        buttonColor={theme.colors.primaryCTA}
+        onPress={primaryOnPress}>
+        {primaryTitle}
+      </Button>
+    </View>
   );
 }
-const getStyles = theme =>
+const getStyles = (theme, width) =>
   StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    seconadryTitleStyle: {
+      color: theme.colors.primaryCTA,
+      marginRight: wp(10),
+    },
     ctaContainerStyle: {
       borderRadius: 10,
+      marginVertical: hp(20),
+      width: width,
     },
     labelStyle: {
       minWidth: wp(120),
