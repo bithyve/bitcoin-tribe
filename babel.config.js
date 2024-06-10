1827989bc2dbce9ea463b5f02decfcb82aa0950e
PraneethGunas <praneethgunas404@gmail.com>
2024-06-10 11:56:07 +0530

    Handled absolute paths for imports

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['optional-require'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          /**
+           * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
+           */
+          '^~(.+)': './src/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+    'optional-require',
+    'react-native-paper/babel',
+  ],
 };
--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from '../screens/Home/Home';
+import HomeScreen from 'src/screens/Home/Home';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function Navigator() {
