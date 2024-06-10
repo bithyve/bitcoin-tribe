eefcf1564d02df33eefc3cb8e8ffaf9cd2973cab
akshay8551 <akshaychavan8551@gmail.com>
2024-06-10 11:57:41 +0530

    Added new nav

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -4,13 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Settings from '../screens/settings/Settings';
 import HomeTabs from './TabNavigation/HomeTabs';
+import Splash from '../screens/splash/Splash';
+import WalletSetupOption from '../screens/onBoarding/WalletSetupOption';
 
 function Navigator() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="WalletSetupOption"
+          component={WalletSetupOption}
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen
           name="Home"
           component={HomeTabs}
