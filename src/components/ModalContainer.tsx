0c91267d41612fe2cedb81c5c5f1a28977ddc60b
akshay8551 <akshaychavan8551@gmail.com>
2024-06-12 14:06:47 +0530

    Modal - done some minor changes

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Modal, useTheme } from 'react-native-paper';
+import { Modal, Portal, Provider, useTheme } from 'react-native-paper';
 
 import IconClose from 'src/assets/images/icon_close.svg';
+import { hp } from 'src/constants/responsive';
 import AppText from './AppText';
 import AppTouchable from './AppTouchable';
 
@@ -19,42 +20,44 @@ const ModalContainer = (props: ModalContainerProps) => {
   const { visible, onDismiss, children, title, subTitle } = props;
   const styles = getStyles(theme);
   return (
-    <Modal
-      visible={visible}
-      onDismiss={onDismiss}
-      contentContainerStyle={styles.containerStyle}>
-      <View>
-        <AppTouchable onPress={onDismiss} style={styles.closeIconWrapper}>
-          <IconClose />
-        </AppTouchable>
-        <View style={styles.headingWrapper}>
-          <AppText
-            variant="heading1"
-            testID="text_modalTitle"
-            style={styles.titleText}>
-            {title}
-          </AppText>
-          <AppText
-            variant="body1"
-            testID="text_modalSubTitle"
-            style={styles.subTitleText}>
-            {subTitle}
-          </AppText>
+    // <Provider>
+    <Portal>
+      <Modal
+        visible={visible}
+        onDismiss={onDismiss}
+        contentContainerStyle={styles.containerStyle}>
+        <View>
+          <AppTouchable onPress={onDismiss} style={styles.closeIconWrapper}>
+            <IconClose />
+          </AppTouchable>
+          <View style={styles.headingWrapper}>
+            <AppText
+              variant="heading1"
+              testID="text_modalTitle"
+              style={styles.titleText}>
+              {title}
+            </AppText>
+            <AppText
+              variant="body1"
+              testID="text_modalSubTitle"
+              style={styles.subTitleText}>
+              {subTitle}
+            </AppText>
+          </View>
+          {children}
         </View>
-        {children}
-      </View>
-    </Modal>
+      </Modal>
+    </Portal>
+    // </Provider>
   );
 };
 const getStyles = theme =>
   StyleSheet.create({
     containerStyle: {
       backgroundColor: theme.colors.cardBackground,
-      padding: 20,
-      margin: 10,
-      zIndex: 999,
-      position: 'absolute',
-      bottom: 100,
+      padding: 25,
+      borderRadius: 10,
+      top: hp(200),
     },
     closeIconWrapper: {
       alignSelf: 'flex-end',
