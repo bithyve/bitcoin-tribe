4ae4d1b44e2f01abf2a898e581b594518cb0f16e
akshay8551 <akshaychavan8551@gmail.com>
2024-06-13 17:57:21 +0530

    formatted

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -5,6 +5,9 @@ import { useTheme } from 'react-native-paper';
 import AppHeader from 'src/components/AppHeader';
 import ScreenContainer from 'src/components/ScreenContainer';
 import SelectOption from 'src/components/SelectOption';
+import { hp, windowHeight } from 'src/constants/responsive';
+import { LocalizationContext } from 'src/contexts/LocalizationContext';
+import { AppTheme } from 'src/theme';
 
 import IconBiometric from 'src/assets/images/icon_fingerprint.svg';
 import IconDarkMode from 'src/assets/images/icon_moon.svg';
@@ -12,9 +15,6 @@ import IconBackup from 'src/assets/images/icon_backup.svg';
 import IconLangCurrency from 'src/assets/images/icon_globe.svg';
 import IconAppInfo from 'src/assets/images/icon_info.svg';
 import IconNodes from 'src/assets/images/icon_node.svg';
-import { hp, windowHeight } from 'src/constants/responsive';
-import { LocalizationContext } from 'src/contexts/LocalizationContext';
-import { AppTheme } from 'src/theme';
 
 function SettingsScreen() {
   const { translations } = useContext(LocalizationContext);
