1abc0b3217a37e9d0f17ce1e1b5a16d7c04ebd15
akshay8551 <akshaychavan8551@gmail.com>
2024-06-14 14:38:54 +0530

    added nav types

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -10,6 +10,8 @@ export type AppStackParams = {
   [NavigationRoutes.SPLASH]: undefined;
   [NavigationRoutes.WALLETDETAILS]: undefined;
   [NavigationRoutes.WALLETSETUPOPTION]: undefined;
+  [NavigationRoutes.WALLETALLTRANSACTION]: undefined;
+  [NavigationRoutes.TRANSACTIONDETAILS]: undefined;
   // Add other routes as needed
 };
 
