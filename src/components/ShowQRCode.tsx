3707b65f63a9db3df4b7f2acf9be522f6ecaed92
nishanttech541 <nishanttech541@gmail.com>
2024-06-14 15:25:56 +0530

    removed hard coded hex color values

--- src/components/ShowQRCode.tsx
+++ src/components/ShowQRCode.tsx
@@ -1,53 +1,54 @@
-import React from 'react'
-import { StyleSheet,View,Image,Text } from 'react-native'
-import { useTheme } from 'react-native-paper'
-import CommonStyles from 'src/common/styles/CommonStyles'
-import { wp } from 'src/constants/responsive'
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import CommonStyles from 'src/common/styles/CommonStyles';
+import { wp } from 'src/constants/responsive';
 
 type ShowQRCodeProps = {
-    icon: any;
-    title: string;
+  icon: any;
+  title: string;
 };
 
-const ShowQRCode = (props:ShowQRCodeProps) => {
+const ShowQRCode = (props: ShowQRCodeProps) => {
   const theme = useTheme();
   const styles = React.useMemo(() => getStyles(theme), [theme]);
-  const { icon, title} = props;
+  const { icon, title } = props;
   return (
     <View style={styles.qrViewWrapper}>
-        <View style={styles.qrImageWrapper}>
-           <Image source={icon} style={styles.qrImageContainer} />
-        </View>
-        <Text style={styles.qrFooterText}>{title}</Text>
+      <View style={styles.qrImageWrapper}>
+        <Image source={icon} style={styles.qrImageContainer} />
+      </View>
+      <Text style={styles.qrFooterText}>{title}</Text>
     </View>
-  )
-}
+  );
+};
 
-const getStyles = theme =>StyleSheet.create({
+const getStyles = theme =>
+  StyleSheet.create({
     qrViewWrapper: {
-        alignSelf:'center',
-        backgroundColor: theme.colors.cardBackground,
-        alignItems: 'center',
-        marginTop:wp(55),
-        borderBottomLeftRadius: 10,
-        borderBottomRightRadius: 10,
-        width: 200,
-      },
-      qrImageContainer:{
-         width: 200,
-         height: 180
-      },
-      qrImageWrapper: {
-        width: 200,
-        height: 180,
-        backgroundColor: '#fff',
-      },
-      qrFooterText: {
-        textAlign: 'center',
-        color: '#FFBA00',
-        fontSize: CommonStyles.body2.fontSize,
-        paddingVertical: 4,
-      },
-})
+      alignSelf: 'center',
+      backgroundColor: theme.colors.cardBackground,
+      alignItems: 'center',
+      marginTop: wp(55),
+      borderBottomLeftRadius: 10,
+      borderBottomRightRadius: 10,
+      width: 200,
+    },
+    qrImageContainer: {
+      width: 200,
+      height: 180,
+    },
+    qrImageWrapper: {
+      width: 200,
+      height: 180,
+      backgroundColor: theme.colors.headingColor,
+    },
+    qrFooterText: {
+      textAlign: 'center',
+      color: theme.colors.accent1,
+      fontSize: CommonStyles.body2.fontSize,
+      paddingVertical: 4,
+    },
+  });
 
-export default ShowQRCode
+export default ShowQRCode;
