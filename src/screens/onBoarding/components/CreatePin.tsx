ad06a4fd8075623665173a0ac2db9ac28b603186
akshay8551 <akshaychavan8551@gmail.com>
2024-06-14 12:56:08 +0530

    Designed Create pin container #46

---
+++ src/screens/onBoarding/components/CreatePin.tsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+import { useTheme } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+
+import { AppTheme } from 'src/theme';
+import Buttons from 'src/components/Buttons';
+import PinInputsView from 'src/components/PinInputsView';
+import { wp } from 'src/constants/responsive';
+import KeyPadView from 'src/components/KeyPadView';
+import DeleteIcon from 'src/assets/images/delete.svg';
+import AppText from 'src/components/AppText';
+
+function CreatePin() {
+  const theme: AppTheme = useTheme();
+  const styles = getStyles(theme);
+  const [passcode, setPasscode] = useState('');
+  const [confirmPasscode, setConfirmPasscode] = useState('');
+  const [passcodeFlag, setPasscodeFlag] = useState(true);
+  const [confirmPasscodeFlag, setConfirmPasscodeFlag] = useState(0);
+
+  function onPressNumber(text) {
+    let tmpPasscode = passcode;
+    let tmpConfirmPasscode = confirmPasscode;
+    if (passcodeFlag) {
+      if (passcode.length < 4) {
+        if (text !== 'x') {
+          tmpPasscode += text;
+          setPasscode(tmpPasscode);
+        }
+      } else if (passcode.length === 4 && passcodeFlag) {
+        setPasscodeFlag(false);
+        setConfirmPasscodeFlag(1);
+        setPasscode(passcode);
+      }
+      if (passcode && text === 'x') {
+        const passcodeTemp = passcode.slice(0, -1);
+        setPasscode(passcodeTemp);
+        if (passcodeTemp.length === 0) {
+          setConfirmPasscodeFlag(0);
+        }
+      }
+    } else if (confirmPasscodeFlag) {
+      if (confirmPasscode.length < 4) {
+        if (text !== 'x') {
+          tmpConfirmPasscode += text;
+          setConfirmPasscode(tmpConfirmPasscode);
+        }
+      }
+      if (confirmPasscode && text === 'x') {
+        setConfirmPasscode(confirmPasscode.slice(0, -1));
+      } else if (!confirmPasscode && text === 'x') {
+        setPasscodeFlag(true);
+        setConfirmPasscodeFlag(0);
+        setConfirmPasscode(confirmPasscode);
+      }
+    }
+  }
+
+  const onDeletePressed = text => {
+    if (passcodeFlag) {
+      setPasscode(passcode.slice(0, -1));
+    } else {
+      setConfirmPasscode(confirmPasscode.slice(0, confirmPasscode.length - 1));
+    }
+  };
+
+  useEffect(() => {
+    if (
+      confirmPasscode.length <= 4 &&
+      confirmPasscode.length > 0 &&
+      passcode.length === 4
+    ) {
+      setPasscodeFlag(false);
+      setConfirmPasscodeFlag(2);
+    } else if (passcode.length === 4 && confirmPasscodeFlag !== 2) {
+      setPasscodeFlag(false);
+      setConfirmPasscodeFlag(1);
+    } else if (
+      !confirmPasscode &&
+      passcode.length > 0 &&
+      passcode.length <= 4 &&
+      confirmPasscodeFlag === 2
+    ) {
+      setPasscodeFlag(true);
+      setConfirmPasscodeFlag(0);
+    } else if (
+      !confirmPasscode &&
+      passcode.length > 0 &&
+      passcode.length <= 4
+    ) {
+      setPasscodeFlag(true);
+      setConfirmPasscodeFlag(0);
+    }
+  }, [passcode, confirmPasscode]);
+
+  return (
+    <View style={styles.container}>
+      <AppText variant="body1" style={styles.labelText}>
+        Enter a Pin
+      </AppText>
+      <PinInputsView passCode={passcode} />
+      {passcode.length === 4 ? (
+        <View>
+          <AppText variant="body1" style={styles.labelText}>
+            Confirm your pin
+          </AppText>
+          <PinInputsView passCode={confirmPasscode} />
+        </View>
+      ) : null}
+      <Buttons
+        primaryTitle="Proceed"
+        primaryOnPress={() => console.log('primary')}
+        secondaryTitle="Cancel"
+        secondaryOnPress={() => console.log('secondary')}
+        width={wp(120)}
+      />
+      <KeyPadView
+        onPressNumber={onPressNumber}
+        onDeletePressed={onDeletePressed}
+        keyColor={theme.colors.primaryCTA}
+        ClearIcon={<DeleteIcon />}
+      />
+    </View>
+  );
+}
+const getStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    container: {
+      width: '100%',
+    },
+    labelText: {
+      color: theme.colors.accent3,
+    },
+  });
+export default CreatePin;
