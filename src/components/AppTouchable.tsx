d83f7e6320dddaa81f19ea264d2c0f626395f279
akshay8551 <akshaychavan8551@gmail.com>
2024-06-17 10:51:35 +0530

    Added disable and backcolor props #47

--- src/components/AppTouchable.tsx
+++ src/components/AppTouchable.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 const AppTouchable = (props: TouchableOpacityProps) => {
-  const { children, style, onPress, testID } = props;
+  const { children, style, onPress, testID, disabled = false } = props;
   return (
     <TouchableOpacity
+      disabled={disabled}
       style={style}
       onPress={onPress}
       activeOpacity={0.7}
