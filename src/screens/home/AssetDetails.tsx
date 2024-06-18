14c083a2b87be489e539698cacaec13715ffda3c
akshay8551 <akshaychavan8551@gmail.com>
2024-06-18 07:14:31 +0530

    Asset details - add margin

--- src/screens/home/AssetDetails.tsx
+++ src/screens/home/AssetDetails.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 import ScreenContainer from 'src/components/ScreenContainer';
 import AppHeader from 'src/components/AppHeader';
 import AssetDetailsContainer from './components/AssetDetailsContainer';
+import { AppTheme } from 'src/theme';
+import { hp, wp } from 'src/constants/responsive';
 
 function AssetDetails() {
+  const theme: AppTheme = useTheme();
+  const styles = React.useMemo(() => getStyles(theme), [theme]);
   return (
-    <ScreenContainer>
-      <AppHeader enableBack={true} />
+    <ScreenContainer style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <AppHeader enableBack={true} />
+      </View>
       <AssetDetailsContainer tag="COLLECTIBLES" />
     </ScreenContainer>
   );
 }
-
+const getStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    container: {
+      padding: 0,
+    },
+    headerWrapper: {
+      marginHorizontal: hp(20),
+      marginTop: hp(15),
+    },
+  });
 export default AssetDetails;
