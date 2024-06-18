93fa5d5e923bcbe8d493fc8d0783239deddebb42
akshay8551 <akshaychavan8551@gmail.com>
2024-06-18 13:38:12 +0530

    Wallet Details - added padding

--- src/screens/wallet/WalletDetails.tsx
+++ src/screens/wallet/WalletDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import ScreenContainer from 'src/components/ScreenContainer';
+import { wp } from 'src/constants/responsive';
 import WalletDetailsHeader from './components/WalletDetailsHeader';
 import WalletTransactionsContainer from './components/WalletTransactionsContainer';
 
@@ -25,14 +26,19 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     height: '100%',
+    padding: 0,
   },
   walletHeaderWrapper: {
-    height: '35%',
+    height: '40%',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: wp(20),
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'gray',
   },
   walletTransWrapper: {
-    height: '64%',
+    height: '58%',
+    marginHorizontal: wp(20),
   },
 });
 export default WalletDetails;
