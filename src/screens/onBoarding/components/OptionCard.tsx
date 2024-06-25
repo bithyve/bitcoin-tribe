b69fc5948f4d5ea1fa2eb86e1172cd14ae1e60a2
akshay8551 <akshaychavan8551@gmail.com>
2024-06-25 17:29:37 +0530

    minor

--- src/screens/onBoarding/components/OptionCard.tsx
+++ src/screens/onBoarding/components/OptionCard.tsx
@@ -51,14 +51,14 @@ const getStyles = (theme: AppTheme) =>
       borderRadius: 10,
       backgroundColor: theme.colors.cardBackground,
       // need to work
-      shadowColor: theme.colors.cardShadowColor,
-      shadowRadius: 10,
-      shadowOpacity: 1,
-      elevation: 1,
-      shadowOffset: {
-        width: 0,
-        height: 4,
-      },
+      // shadowColor: theme.colors.cardShadowColor,
+      // shadowRadius: 10,
+      // shadowOpacity: 1,
+      // elevation: 2,
+      // shadowOffset: {
+      //   width: 0,
+      //   height: 4,
+      // },
     },
     detailsWrapper: {
       flexDirection: 'row',
