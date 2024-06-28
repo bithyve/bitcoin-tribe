f9c7489fa601983f5c61bfbb318200ed9f8e8464
akshay8551 <akshaychavan8551@gmail.com>
2024-06-28 13:23:16 +0530

    Settings - Used array for map the options (Resolved comments)

--- src/screens/settings/ConnectionSettings.tsx
+++ src/screens/settings/ConnectionSettings.tsx
@@ -1,34 +1,45 @@
-import React, { useContext } from 'react';
+import React, { ReactNode, useContext } from 'react';
 
 import AppHeader from 'src/components/AppHeader';
 import ScreenContainer from 'src/components/ScreenContainer';
-import SelectOption from 'src/components/SelectOption';
 import { LocalizationContext } from 'src/contexts/LocalizationContext';
-
 import IconTor from 'src/assets/images/icon_tor.svg';
 import IconNodes from 'src/assets/images/icon_node.svg';
+import SettingMenuItem from './components/SettingMenuItem';
 
+type ConnectionSettingMenuProps = {
+  title: string;
+  subtitle: string;
+  icon: ReactNode;
+  onPress: () => void;
+};
 function ConnectionSettings() {
   const { translations } = useContext(LocalizationContext);
   const { settings } = translations;
+
+  const ConnectionSettingMenu: ConnectionSettingMenuProps[] = [
+    {
+      title: settings.tor,
+      subtitle: settings.torSubTitle,
+      icon: <IconTor />,
+      onPress: () => console.log('tor pressed'),
+    },
+    {
+      title: settings.NodeSettings,
+      subtitle: settings.NodeSettingSubTitle,
+      icon: <IconNodes />,
+      onPress: () => console.log('node setting pressed'),
+    },
+    // Add more menu items as needed
+  ];
+
   return (
     <ScreenContainer>
       <AppHeader
         title={settings.connectionSettings}
         subTitle={settings.conSettingScreenSubTitle}
       />
-      <SelectOption
-        title={settings.tor}
-        subTitle={settings.torSubTitle}
-        icon={<IconTor />}
-        onPress={() => {}}
-      />
-      <SelectOption
-        title={settings.NodeSettings}
-        subTitle={settings.NodeSettingSubTitle}
-        icon={<IconNodes />}
-        onPress={() => console.log('press')}
-      />
+      <SettingMenuItem SettingsMenu={ConnectionSettingMenu} />
     </ScreenContainer>
   );
 }
