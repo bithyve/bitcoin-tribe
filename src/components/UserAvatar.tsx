f6f5aea0d24b158d42796642f18c2bdd381292a9
akshay8551 <akshaychavan8551@gmail.com>
2024-07-02 16:59:45 +0530

    User Avatar - changed profile back color

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { Avatar } from 'react-native-paper';
+import { Avatar, useTheme } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { AppTheme } from 'src/theme';
 
 type UserAvatarProps = {
   size: number;
   imageSource: any;
 };
 const UserAvatar = (props: UserAvatarProps) => {
+  const theme: AppTheme = useTheme();
+  const styles = getStyles(theme);
   const { size, imageSource } = props;
   return (
     <Avatar.Image
@@ -15,7 +19,14 @@ const UserAvatar = (props: UserAvatarProps) => {
           ? 'file://' + imageSource
           : 'https://gravatar.com/avatar/a7ef0d47358b93336c4451de121be367?s=400&d=robohash&r=x',
       }}
+      style={styles.wrapper}
     />
   );
 };
+const getStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    wrapper: {
+      backgroundColor: theme.colors.profileBackground,
+    },
+  });
 export default UserAvatar;
