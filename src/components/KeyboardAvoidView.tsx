f89600dcaae28b8526e083c14d650ee447c1e26f
akshay8551 <akshaychavan8551@gmail.com>
2024-07-08 11:21:50 +0530

    Resolved - Keyboard avoid view issue

--- src/components/KeyboardAvoidView.tsx
+++ src/components/KeyboardAvoidView.tsx
@@ -6,17 +6,19 @@ import {
   ScrollView,
 } from 'react-native';
 import { useTheme } from 'react-native-paper';
+import { windowHeight } from 'src/constants/responsive';
 
 import { AppTheme } from 'src/theme';
 
 const KeyboardAvoidView = props => {
+  console.log('windowHeight', windowHeight)
   const theme: AppTheme = useTheme();
   const styles = React.useMemo(() => getStyles(theme), [theme]);
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : null}
       enabled
-      keyboardVerticalOffset={Platform.select({ ios: 40, android: 500 })}
+      keyboardVerticalOffset={Platform.select({ ios: windowHeight>670? 40 : 5 , android: 500 })}
       style={{ ...styles.container, ...props.style }}>
       <ScrollView
         showsVerticalScrollIndicator={false}
@@ -30,8 +32,7 @@ const KeyboardAvoidView = props => {
 const getStyles = (theme: AppTheme) =>
   StyleSheet.create({
     container: {
-      height: Platform.OS == 'ios' ? 'auto' : 0,
-      flex: Platform.OS == 'ios' ? 0 : 1,
+      flex: 1,
     },
   });
 export default KeyboardAvoidView;
