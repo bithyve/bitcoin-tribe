48e3941f7b6c00e48db8f57183b4ac5e8c9f3e7e
akshay8551 <akshaychavan8551@gmail.com>
2024-07-10 18:37:00 +0530

    Removed unused code

--- src/screens/wallet/components/ShowXPubContainer.tsx
+++ src/screens/wallet/components/ShowXPubContainer.tsx
@@ -1,34 +1,15 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 
-import FooterNote from 'src/components/FooterNote';
+// import FooterNote from 'src/components/FooterNote';
 import { LocalizationContext } from 'src/contexts/LocalizationContext';
 import IconCopy from 'src/assets/images/icon_copy.svg';
-import WalletUtilities from 'src/services/wallets/operations/utils';
 import ShowQRCode from 'src/components/ShowQRCode';
 import ReceiveQrClipBoard from 'src/screens/receive/components/ReceiveQrClipBoard';
 
 function ShowXPubContainer({ xpub }) {
-  const receivingAddress = 'address';
   const { translations } = useContext(LocalizationContext);
-  const { receciveScreen, common, wallet } = translations;
-
-  const [amount, setAmount] = useState(0);
-  const [paymentURI, setPaymentURI] = useState(null);
-
-  useEffect(() => {
-    if (amount) {
-      const newPaymentURI = WalletUtilities.generatePaymentURI(
-        receivingAddress,
-        {
-          amount: parseInt(amount) / 1e8,
-        },
-      ).paymentURI;
-      setPaymentURI(newPaymentURI);
-    } else if (paymentURI) {
-      setPaymentURI(null);
-    }
-  }, [amount, receivingAddress]);
+  const { wallet } = translations;
 
   return (
     <View style={styles.container}>
