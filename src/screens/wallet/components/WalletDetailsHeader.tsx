4eaa1e3e1aaea5923d3307381df6bfe8f18cc077
Parsh <parsh.cosmos11@gmail.com>
2024-07-19 11:17:46 +0530

    Receive screen: rewired w/ change address for RGB

--- src/screens/wallet/components/WalletDetailsHeader.tsx
+++ src/screens/wallet/components/WalletDetailsHeader.tsx
@@ -34,7 +34,8 @@ function WalletDetailsHeader(props: walletDetailsHeaderProps) {
       balances: { confirmed: 0, unconfirmed: 0 },
     },
   } = wallet;
-  const receivingAddress = WalletOperations.getNextFreeAddress(wallet);
+  const { changeAddress: receivingAddress } =
+    WalletOperations.getNextFreeChangeAddress(wallet);
 
   return (
     <View style={styles.container}>
