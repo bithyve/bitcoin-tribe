6d310f3330cdececb249056f6d28b2cf5a74bbcc
akshay8551 <akshaychavan8551@gmail.com>
2024-08-07 16:09:23 +0530

    Done - All the settings should be in a single scroll view.

--- src/components/SelectOption.tsx
+++ src/components/SelectOption.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useTheme, Switch } from 'react-native-paper';
 
 import IconArrow from 'src/assets/images/icon_right_arrow.svg';
 import IconSettingArrow from 'src/assets/images/icon_arrowr2.svg';
 import AppText from './AppText';
 import AppTouchable from './AppTouchable';
-import Switch from './Switch';
 import { AppTheme } from 'src/theme';
 import { hp, windowHeight } from 'src/constants/responsive';
 import GradientView from './GradientView';
@@ -40,7 +39,6 @@ const SelectOption = (props: SelectOptionProps) => {
     showArrow = true,
   } = props;
   const styles = getStyles(theme, backColor);
-
   return (
     <AppTouchable onPress={onPress}>
       <GradientView
@@ -67,9 +65,9 @@ const SelectOption = (props: SelectOptionProps) => {
           <View>
             {enableSwitch ? (
               <Switch
-                onValueChange={onValueChange}
                 value={toggleValue}
-                testID={testID}
+                onValueChange={onValueChange}
+                color={theme.colors.accent1}
               />
             ) : backColor ? (
               <IconArrow />
--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, ReactNode } from 'react';
+import React, { useContext, ReactNode, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
@@ -11,16 +11,25 @@ import IconBackup from 'src/assets/images/icon_backup.svg';
 import IconLangCurrency from 'src/assets/images/icon_globe1.svg';
 import IconAppInfo from 'src/assets/images/icon_info.svg';
 import IconNodes from 'src/assets/images/icon_node.svg';
+import IconBiometric from 'src/assets/images/icon_fingerprint.svg';
+import IconDarkMode from 'src/assets/images/icon_moon.svg';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
 import AppText from 'src/components/AppText';
 import SettingMenuContainer from './components/SettingMenuContainer';
 import SettingMenuItem from './components/SettingMenuItem';
+import { useMMKVBoolean } from 'react-native-mmkv';
+import { Keys } from 'src/storage';
 
 type SettingMenuProps = {
+  id: number;
   title: string;
   subtitle?: string;
   icon: ReactNode;
-  onPress: () => void;
+  onPress?: () => void;
+  enableSwitch?: boolean;
+  toggleValue?: boolean;
+  onValueChange?: () => void;
+  testID?: string;
 };
 
 function SettingsScreen({ navigation }) {
@@ -29,28 +38,49 @@ function SettingsScreen({ navigation }) {
   const theme: AppTheme = useTheme();
   const styles = getStyles(theme);
 
+  const [darkTheme, setDarkTheme] = useMMKVBoolean(Keys.THEME_MODE);
+  const [biometrics, setBiometrics] = useState(false);
+
   const SettingsMenu: SettingMenuProps[] = [
     {
+      id: 1,
+      title: settings.darkMode,
+      icon: <IconDarkMode />,
+      onValueChange: () => setDarkTheme(!darkTheme),
+      toggleValue: !darkTheme,
+      enableSwitch: true,
+      testID: 'dark_mode',
+    },
+    {
+      id: 2,
+      title: settings.biometricUnlock,
+      icon: <IconBiometric />,
+      onValueChange: () => setBiometrics(!biometrics),
+      toggleValue: !biometrics,
+      enableSwitch: true,
+      testID: 'biometric_unlock',
+    },
+    {
+      id: 3,
       title: settings.langAndCurrency,
-      // subtitle: settings.langAndCurrencySubTitle,
       icon: <IconLangCurrency />,
       onPress: () => navigation.navigate(NavigationRoutes.LANGUAGEANDCURRENCY),
     },
     {
+      id: 4,
       title: settings.appBackup,
-      // subtitle: settings.appBackupSubTitle,
       icon: <IconBackup />,
       onPress: () => navigation.navigate(NavigationRoutes.APPBACKUPMENU),
     },
     {
+      id: 5,
       title: settings.connectionSettings,
-      // subtitle: settings.connectionSettingSubTitle,
       icon: <IconNodes />,
       onPress: () => navigation.navigate(NavigationRoutes.CONNECTIONSETTINGS),
     },
     {
+      id: 6,
       title: settings.appInfo,
-      // subtitle: settings.appInfoSubTitle,
       icon: <IconAppInfo />,
       onPress: () => navigation.navigate(NavigationRoutes.APPINFO),
     },
@@ -62,7 +92,7 @@ function SettingsScreen({ navigation }) {
       <AppText variant="pageTitle2" style={styles.title}>
         {settings.setting}
       </AppText>
-      <SettingMenuContainer />
+      {/* <SettingMenuContainer /> */}
       <SettingMenuItem SettingsMenu={SettingsMenu} />
     </ScreenContainer>
   );
