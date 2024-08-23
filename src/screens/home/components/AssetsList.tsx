31495744139938bd98c154a16ac5e252854bf419
akshay8551 <akshaychavan8551@gmail.com>
2024-08-23 15:19:17 +0530

    minor

--- src/screens/home/components/AssetsList.tsx
+++ src/screens/home/components/AssetsList.tsx
@@ -173,7 +173,7 @@ const getStyles = (theme: AppTheme, index = null) =>
       height: 100, // Adjust the height as needed
     },
     alternateSpace: {
-      // top: index % 2 === 0 ? 0 : hp(50),
+      top: index % 2 === 0 ? 0 : hp(50),
     },
   });
 export default AssetsList;
