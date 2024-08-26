9001d7f6f7b063c69174005c1164ff1e14d7ad76
akshay8551 <akshaychavan8551@gmail.com>
2024-08-26 16:05:30 +0530

    Done - UI - Update Send Flow #198

--- src/screens/send/components/SendToContainer.tsx
+++ src/screens/send/components/SendToContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useTheme } from 'react-native-paper';
+import { RadioButton, useTheme } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -20,6 +20,11 @@ import { useMutation } from 'react-query';
 import { ApiHandler } from 'src/services/handler/apiHandler';
 import SendAddressIcon from 'src/assets/images/sendAddress.svg';
 import { formatNumber } from 'src/utils/numberWithCommas';
+import { useMMKVString } from 'react-native-mmkv';
+import { Keys } from 'src/storage';
+import CurrencyKind from 'src/models/enums/CurrencyKind';
+import useBalance from 'src/hooks/useBalance';
+import { TxPriority } from 'src/services/wallets/enums';
 
 function SendToContainer({
   wallet,
@@ -31,12 +36,18 @@ function SendToContainer({
   paymentURIAmount: Number;
 }) {
   const navigation = useNavigation();
+  const { getBalance, getCurrencyIcon } = useBalance();
   const theme: AppTheme = useTheme();
   const { translations } = useContext(LocalizationContext);
   const { common, sendScreen } = translations;
+  const [currentCurrencyMode] = useMMKVString(Keys.CURRENCY_MODE);
+  const initialCurrencyMode = currentCurrencyMode || CurrencyKind.SATS;
   const styles = React.useMemo(() => getStyles(theme), [theme]);
   const [amount, setAmount] = useState(
     paymentURIAmount ? `${paymentURIAmount}` : '',
+  );
+  const [selectedPriority, setSelectedPriority] = React.useState(
+    TxPriority.LOW,
   );
   const [insufficientBalance, setInsufficientBalance] = useState(false);
 
@@ -118,32 +129,117 @@ function SendToContainer({
           value={formatNumber(amount)}
           onChangeText={text => setAmount(text)}
           placeholder={sendScreen.enterAmount}
-          // keyboardType={'default'}
-          disabled={true}
+          keyboardType={'numeric'}
+          // disabled={true}
           icon={<IconBitcoin />}
           // rightText={common.sendMax}
           // onRightTextPress={() => {}}
           // rightCTATextColor={theme.colors.accent1}
         />
+        <View style={styles.totalFeeWrapper}>
+          <AppText variant="heading1" style={styles.feeTitleText}>
+            Total Fee:
+          </AppText>
+          {initialCurrencyMode !== CurrencyKind.SATS
+            ? getCurrencyIcon(IconBitcoin, 'dark')
+            : null}
+          <AppText variant="heading1" style={styles.amountText}>
+            &nbsp;{getBalance(2000)}
+          </AppText>
+          {initialCurrencyMode === CurrencyKind.SATS && (
+            <AppText variant="caption" style={styles.satsText}>
+              sats
+            </AppText>
+          )}
+        </View>
+        <View style={styles.feeWrapper}>
+          <View style={styles.radioBtnWrapper}>
+            <RadioButton.Android
+              color={theme.colors.accent1}
+              uncheckedColor={theme.colors.headingColor}
+              value={TxPriority.LOW}
+              status={
+                selectedPriority === TxPriority.LOW ? 'checked' : 'unchecked'
+              }
+              onPress={() => setSelectedPriority(TxPriority.LOW)}
+            />
+            <View style={styles.feeViewWrapper}>
+              <AppText variant="body2" style={styles.feePriorityText}>
+                Low -
+              </AppText>
+              <AppText variant="body2" style={styles.feeText}>
+                &nbsp;1 sat/vbyte
+              </AppText>
+              <AppText variant="caption" style={styles.feeSatsText}>
+                ~3hours
+              </AppText>
+            </View>
+          </View>
+          <View style={styles.radioBtnWrapper}>
+            <RadioButton.Android
+              color={theme.colors.accent1}
+              uncheckedColor={theme.colors.headingColor}
+              value={TxPriority.MEDIUM}
+              status={
+                selectedPriority === TxPriority.MEDIUM ? 'checked' : 'unchecked'
+              }
+              onPress={() => setSelectedPriority(TxPriority.MEDIUM)}
+            />
+            <View style={styles.feeViewWrapper}>
+              <AppText variant="body2" style={styles.feePriorityText}>
+                Medium -
+              </AppText>
+              <AppText variant="body2" style={styles.feeText}>
+                &nbsp;1 sat/vbyte
+              </AppText>
+              <AppText variant="caption" style={styles.feeSatsText}>
+                ~3hours
+              </AppText>
+            </View>
+          </View>
+          <View style={styles.radioBtnWrapper}>
+            <RadioButton.Android
+              color={theme.colors.accent1}
+              uncheckedColor={theme.colors.headingColor}
+              value={TxPriority.HIGH}
+              status={
+                selectedPriority === TxPriority.HIGH ? 'checked' : 'unchecked'
+              }
+              onPress={() => setSelectedPriority(TxPriority.HIGH)}
+            />
+            <View style={styles.feeViewWrapper}>
+              <AppText variant="body2" style={styles.feePriorityText}>
+                High -
+              </AppText>
+              <AppText variant="body2" style={styles.feeText}>
+                &nbsp;1 sat/vbyte
+              </AppText>
+              <AppText variant="caption" style={styles.feeSatsText}>
+                ~3hours
+              </AppText>
+            </View>
+          </View>
+        </View>
       </View>
+
       <View style={styles.primaryCTAContainer}>
         <Buttons
           disabled={!amount}
-          primaryTitle={common.proceed}
+          primaryTitle={common.broadcast}
           secondaryTitle={common.cancel}
           primaryOnPress={initiateSendPhaseOne}
           secondaryOnPress={navigation.goBack}
           width={wp(120)}
         />
       </View>
-      <View style={styles.keyPadWrapper}>
+      {/* <View style={styles.keyPadWrapper}>
         <KeyPadView
           onPressNumber={onPressNumber}
           onDeletePressed={onDeletePressed}
           keyColor={theme.colors.accent1}
           ClearIcon={<DeleteIcon />}
         />
-      </View>
+      </View> */}
     </View>
   );
 }
@@ -183,6 +279,44 @@ const getStyles = (theme: AppTheme) =>
     },
     txnID: {
       color: theme.colors.secondaryHeadingColor,
+    },
+    totalFeeWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: hp(20),
+    },
+    feeTitleText: {
+      color: theme.colors.headingColor,
+      marginRight: hp(10),
+    },
+    amountText: {
+      // marginLeft: hp(5),
+      color: theme.colors.headingColor,
+    },
+    satsText: {
+      color: theme.colors.headingColor,
+      marginTop: hp(5),
+      marginLeft: hp(5),
+    },
+    radioBtnWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: hp(10),
+    },
+    feeViewWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    feePriorityText: {
+      color: theme.colors.headingColor,
+      marginRight: hp(10),
+    },
+    feeText: {
+      color: theme.colors.headingColor,
+    },
+    feeSatsText: {
+      color: theme.colors.headingColor,
+      marginLeft: hp(5),
     },
   });
 export default SendToContainer;
