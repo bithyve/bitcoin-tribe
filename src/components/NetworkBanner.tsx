fd5e603dd0a3ad07a8ec0913d52e8644ada8c7f2
akshay8551 <akshaychavan8551@gmail.com>
2024-10-14 17:00:55 +0530

    import color

--- src/components/NetworkBanner.tsx
+++ src/components/NetworkBanner.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Platform } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LocalizationContext } from 'src/contexts/LocalizationContext';
+import Colors from 'src/theme/Colors';
 
 const NetworkBanner = () => {
   const [isConnected, setIsConnected] = useState(true);
@@ -30,14 +31,14 @@ const NetworkBanner = () => {
 
 const styles = StyleSheet.create({
   safeArea: {
-    backgroundColor: 'red', // SafeAreaView with red background to match the banner
+    backgroundColor: Colors.ImperialRed, // SafeAreaView with red background to match the banner
   },
   banner: {
     position: 'absolute',
     top: Platform.OS === 'ios' ? 25 : 8,
     left: 0,
     right: 0,
-    backgroundColor: 'red',
+    backgroundColor: Colors.ImperialRed,
     padding: 20,
     zIndex: 1000, // Ensures the banner is above everything
     alignItems: 'center',
