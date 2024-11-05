add39494e1c8da3bc983ddd0677a537b80879ba6
akshay8551 <akshaychavan8551@gmail.com>
2024-11-05 20:00:52 +0530

    Channel management screen #494

--- src/screens/channels/RgbChannels.tsx
+++ src/screens/channels/RgbChannels.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useContext } from 'react';
 import ScreenContainer from 'src/components/ScreenContainer';
 import AppHeader from 'src/components/AppHeader';
 import PlusIcon from 'src/assets/images/plus.svg';
@@ -8,21 +8,26 @@ import { useNavigation } from '@react-navigation/native';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
 import { useMMKVBoolean } from 'react-native-mmkv';
 import { Keys } from 'src/storage';
+import RGBChannelContainer from './components/RGBChannelContainer';
+import { LocalizationContext } from 'src/contexts/LocalizationContext';
 
 const styles = StyleSheet.create({});
 
 const RgbChannels = () => {
   const navigation = useNavigation();
   const [isThemeDark] = useMMKVBoolean(Keys.THEME_MODE);
+  const { translations } = useContext(LocalizationContext);
+  const { node } = translations;
   return (
     <ScreenContainer>
       <AppHeader
-        title={'Channels'}
+        title={node.channelsTitle}
         rightIcon={!isThemeDark ? <PlusIcon /> : <PlusLightIcon />}
         onSettingsPress={() =>
           navigation.navigate(NavigationRoutes.OPENRGBCHANNEL)
         }
       />
+      <RGBChannelContainer />
     </ScreenContainer>
   );
 };
