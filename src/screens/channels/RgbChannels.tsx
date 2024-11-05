e7291e311e67e1e85adf8dc8a27d3f486497833a
akshay8551 <akshaychavan8551@gmail.com>
2024-11-05 10:28:51 +0530

    RGB channel - added/import right icon according to theme

--- src/screens/channels/RgbChannels.tsx
+++ src/screens/channels/RgbChannels.tsx
@@ -1,20 +1,24 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React from 'react';
 import ScreenContainer from 'src/components/ScreenContainer';
 import AppHeader from 'src/components/AppHeader';
 import PlusIcon from 'src/assets/images/plus.svg';
+import PlusLightIcon from 'src/assets/images/plus_light.svg';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
+import { useMMKVBoolean } from 'react-native-mmkv';
+import { Keys } from 'src/storage';
 
 const styles = StyleSheet.create({});
 
 const RgbChannels = () => {
   const navigation = useNavigation();
+  const [isThemeDark] = useMMKVBoolean(Keys.THEME_MODE);
   return (
     <ScreenContainer>
       <AppHeader
         title={'Channels'}
-        rightIcon={<PlusIcon />}
+        rightIcon={!isThemeDark ? <PlusIcon /> : <PlusLightIcon />}
         onSettingsPress={() =>
           navigation.navigate(NavigationRoutes.OPENRGBCHANNEL)
         }
