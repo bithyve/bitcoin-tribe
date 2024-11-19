0d577b0b225551f3ed704f0c373b4e6e4b3371e9
akshay8551 <akshaychavan8551@gmail.com>
2024-11-19 11:36:51 +0530

    Done - While creating a wallet (Logo animation keeps on running ) #438

--- src/screens/splash/Splash.tsx
+++ src/screens/splash/Splash.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useContext, useCallback } from 'react';
-import { StyleSheet, ImageBackground, Image } from 'react-native';
+import React, { useEffect, useContext, useCallback, useState } from 'react';
+import { StyleSheet, ImageBackground } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useTheme } from 'react-native-paper';
+import { useMMKVBoolean, useMMKVString } from 'react-native-mmkv';
+import { useMutation } from 'react-query';
+
+import { AppTheme } from 'src/theme';
 import ScreenContainer from 'src/components/ScreenContainer';
 import { AppContext } from 'src/contexts/AppContext';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
 import { Keys, Storage } from 'src/storage';
 import PinMethod from 'src/models/enums/PinMethod';
-import { useMutation } from 'react-query';
 import { ApiHandler } from 'src/services/handler/apiHandler';
-import { AppTheme } from 'src/theme';
-import { useTheme } from 'react-native-paper';
-import { useMMKVBoolean, useMMKVString } from 'react-native-mmkv';
-import TribeIcon from 'src/assets/images/Tribe.svg';
 import dbManager from 'src/storage/realm/dbManager';
 import { RealmSchema } from 'src/storage/enum';
+import { TribeApp } from 'src/models/interfaces/TribeApp';
 
 function Splash({ navigation }) {
   const theme: AppTheme = useTheme();
@@ -21,6 +23,7 @@ function Splash({ navigation }) {
   const { setKey, setIsWalletOnline, setAppType } = useContext(AppContext);
   const { mutate, data } = useMutation(ApiHandler.login);
   const [pinMethod] = useMMKVString(Keys.PIN_METHOD);
+  const [animationFinished, setAnimationFinished] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -61,10 +64,15 @@ function Splash({ navigation }) {
     }
   }, [data, navigation, setKey]);
 
+  // useEffect(() => {
+  //   const timer = setTimeout(onInit, 4000);
+  //   return () => clearTimeout(timer);
+  // }, [onInit]);
   useEffect(() => {
-    const timer = setTimeout(onInit, 4000);
-    return () => clearTimeout(timer);
-  }, [onInit]);
+    if (animationFinished) {
+      onInit();
+    }
+  }, [animationFinished, onInit]);
 
   return (
     <ScreenContainer style={styles.container}>
@@ -76,11 +84,13 @@ function Splash({ navigation }) {
         }
         resizeMode="cover"
         style={styles.backImage}>
-        {/* <Image
-          source={require('src/assets/images/RGB_Splash.gif')}
+        <LottieView
+          source={require('src/assets/images/logoAnimation.json')} // Update with the path to your Lottie JSON file
+          autoPlay
+          loop={false} // Ensures the animation doesn't loop
           style={styles.splashImageStyle}
-        /> */}
-        <TribeIcon />
+          onAnimationFinish={() => setAnimationFinished(true)}
+        />
       </ImageBackground>
     </ScreenContainer>
   );
