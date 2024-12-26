c3808a081b8f659bb13b5c937f6dd5530ce03f9a
akshay8551 <akshaychavan8551@gmail.com>
2024-12-26 11:55:36 +0530

    Send - modified screen #460

--- src/screens/send/SendBTCScreen.tsx
+++ src/screens/send/SendBTCScreen.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useContext, useCallback } from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
+import { Keyboard, Platform, StyleSheet, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { useTheme } from 'react-native-paper';
+import { Code } from 'react-native-vision-camera';
+import Clipboard from '@react-native-clipboard/clipboard';
+
 import AppHeader from 'src/components/AppHeader';
 import ScreenContainer from 'src/components/ScreenContainer';
-import OptionCard from 'src/components/OptionCard';
 import { LocalizationContext } from 'src/contexts/LocalizationContext';
 import QRScanner from 'src/components/QRScanner';
 import { AppTheme } from 'src/theme';
-import { useTheme } from 'react-native-paper';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
 import ModalContainer from 'src/components/ModalContainer';
 import SendEnterAddress from './components/SendEnterAddress';
@@ -14,7 +17,9 @@ import { PaymentInfoKind } from 'src/services/wallets/enums';
 import Toast from 'src/components/Toast';
 import WalletUtilities from 'src/services/wallets/operations/utils';
 import config from 'src/utils/config';
-import { Code } from 'react-native-vision-camera';
+import TextField from 'src/components/TextField';
+import { hp } from 'src/constants/responsive';
+import AppText from 'src/components/AppText';
 
 function SendBTCScreen({ route, navigation }) {
   const theme: AppTheme = useTheme();
@@ -22,7 +27,9 @@ function SendBTCScreen({ route, navigation }) {
   const { sendScreen } = translations;
   const styles = getStyles(theme);
   const [visible, setVisible] = useState(false);
+  const [address, setAddress] = useState('');
   const { receiveData, title, subTitle, wallet } = route.params;
+
   const onCodeScanned = useCallback((codes: Code[]) => {
     const value = codes[0]?.value;
     if (value == null) {
@@ -52,21 +59,74 @@ function SendBTCScreen({ route, navigation }) {
         Toast(sendScreen.invalidBtcAddress, true);
     }
   }, []);
+
+  const handlePasteAddress = async () => {
+    const getClipboardValue = await Clipboard.getString();
+    const network = WalletUtilities.getNetworkByType(config.NETWORK_TYPE);
+    let {
+      type: paymentInfoKind,
+      address,
+      amount,
+    } = WalletUtilities.addressDiff(getClipboardValue, network);
+    if (amount) {
+      amount = Math.trunc(amount * 1e8);
+    } // convert from bitcoins to sats
+    if (paymentInfoKind) {
+      Keyboard.dismiss();
+      setAddress(address);
+      switch (paymentInfoKind) {
+        case PaymentInfoKind.ADDRESS:
+          navigation.replace(NavigationRoutes.SENDTO, { wallet, address });
+          break;
+        case PaymentInfoKind.PAYMENT_URI:
+          navigation.replace(NavigationRoutes.SENDTO, {
+            wallet,
+            address,
+            paymentURIAmount: amount,
+          });
+          break;
+        default:
+          Toast(sendScreen.invalidBtcAddress, true);
+      }
+    } else {
+      Keyboard.dismiss();
+      Toast(sendScreen.invalidBtcAddress, true);
+    }
+  };
+
   return (
     <ScreenContainer>
-      <AppHeader title={title} subTitle={subTitle} enableBack={true} />
-      <View style={styles.scannerWrapper}>
-        {!visible && <QRScanner onCodeScanned={onCodeScanned} />}
-      </View>
-      <OptionCard
-        title={sendScreen.optionCardTitle}
-        subTitle={sendScreen.sendBtcOptionCardSubTitle}
-        onPress={() => {
-          receiveData === 'send'
-            ? setVisible(true)
-            : navigation.navigate(NavigationRoutes.CONNECTNODEMANUALLY);
-        }}
-      />
+      <AppHeader title={title} enableBack={true} />
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.keyboardAwareContainer}
+        contentContainerStyle={styles.keyboardAwareContent}
+        enableOnAndroid={true}
+        keyboardOpeningTime={0}>
+        <View style={styles.scannerWrapper}>
+          {!visible && <QRScanner onCodeScanned={onCodeScanned} />}
+        </View>
+        <View style={styles.inputWrapper}>
+          <AppText variant="body2" style={styles.recipientAddressLabel}>
+            {sendScreen.recipientAddress}
+          </AppText>
+          <TextField
+            value={address}
+            onChangeText={text => setAddress(text)}
+            placeholder={sendScreen.enterAddress}
+            keyboardType={'default'}
+            returnKeyType={'Enter'}
+            multiline={true}
+            numberOfLines={2}
+            inputStyle={styles.inputStyle}
+            contentStyle={styles.contentStyle}
+            rightText={sendScreen.paste}
+            onRightTextPress={() => handlePasteAddress()}
+            rightCTAStyle={styles.rightCTAStyle}
+            rightCTATextColor={theme.colors.accent1}
+          />
+        </View>
+      </KeyboardAwareScrollView>
       <ModalContainer
         title={sendScreen.enterSendAddress}
         subTitle={sendScreen.enterSendAdrsSubTitle}
@@ -81,8 +141,34 @@ function SendBTCScreen({ route, navigation }) {
 }
 const getStyles = (theme: AppTheme) =>
   StyleSheet.create({
+    keyboardAwareContainer: {
+      flex: 1,
+    },
+    keyboardAwareContent: {
+      flexGrow: 1,
+    },
     scannerWrapper: {
       flex: 1,
+    },
+    inputWrapper: {
+      paddingVertical: 16,
+    },
+    recipientAddressLabel: {
+      marginVertical: hp(10),
+      color: theme.colors.secondaryHeadingColor,
+    },
+    inputStyle: {
+      width: '80%',
+    },
+    contentStyle: {
+      marginTop: 0,
+    },
+    rightCTAStyle: {
+      height: hp(40),
+      width: hp(55),
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginHorizontal: hp(5),
     },
   });
 export default SendBTCScreen;
