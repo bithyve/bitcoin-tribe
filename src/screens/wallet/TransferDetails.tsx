7fde7d53d21c03fbeb8be87428d6c07921a424d1
akshay8551 <akshaychavan8551@gmail.com>
2025-01-10 17:17:08 +0530

    Done - Cancel asset-sending transaction when it is in the "Waiting for Counterparty" state.

--- src/screens/wallet/TransferDetails.tsx
+++ src/screens/wallet/TransferDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useTheme } from 'react-native-paper';
 import AppHeader from 'src/components/AppHeader';
 
@@ -7,20 +7,47 @@ import { LocalizationContext } from 'src/contexts/LocalizationContext';
 import { AppTheme } from 'src/theme';
 import { Transaction } from 'src/services/wallets/interfaces';
 import TransferDetailsContainer from './components/TransferDetailsContainer';
+import { ApiHandler } from 'src/services/handler/apiHandler';
+import Toast from 'src/components/Toast';
+import { useMutation } from 'react-query';
+import ModalLoading from 'src/components/ModalLoading';
 
-function TransferDetails({ route }) {
+function TransferDetails({ route, navigation }) {
   const transaction: Transaction = route.params?.transaction;
   const coin = route.params?.coin;
   const { translations } = useContext(LocalizationContext);
-  const { wallet } = translations;
+  const { wallet, assets } = translations;
   const theme: AppTheme = useTheme();
+  const {
+    mutate: cancelTransactionMutation,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useMutation(() =>
+    ApiHandler.handleTransferFailure(transaction.batchTransferIdx, false),
+  );
+
+  useEffect(() => {
+    if (isSuccess) {
+      Toast(assets.cancelTransferMsg);
+      navigation.goBack();
+    } else if (isError) {
+    } else {
+    }
+  }, [isSuccess, isError]);
+
   return (
     <ScreenContainer>
       <AppHeader title={wallet.transferDetails} subTitle={coin} />
-      <TransferDetailsContainer
-        transAmount={`${transaction.amount}`}
-        transaction={transaction}
-      />
+      {isLoading ? (
+        <ModalLoading visible={isLoading} />
+      ) : (
+        <TransferDetailsContainer
+          transAmount={`${transaction.amount}`}
+          transaction={transaction}
+          onPress={() => cancelTransactionMutation()}
+        />
+      )}
     </ScreenContainer>
   );
 }
