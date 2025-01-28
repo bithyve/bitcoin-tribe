eb88fbb0f459c8b266c70b4da3667d80dc279381
shashank <shashank@cakesofttech.com>
2025-01-28 12:52:49 +0530

    pay service fee to issue assets

--- src/screens/home/components/AddAsset.tsx
+++ src/screens/home/components/AddAsset.tsx
@@ -1,8 +1,8 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useContext, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { Platform, StyleSheet, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
-
+import { useMutation } from 'react-query';
 import AppHeader from 'src/components/AppHeader';
 import ResponsePopupContainer from 'src/components/ResponsePopupContainer';
 import ScreenContainer from 'src/components/ScreenContainer';
@@ -10,40 +10,171 @@ import SelectOption from 'src/components/SelectOption';
 import { hp } from 'src/constants/responsive';
 import { LocalizationContext } from 'src/contexts/LocalizationContext';
 import useWallets from 'src/hooks/useWallets';
+import { RgbUnspent, RGBWallet } from 'src/models/interfaces/RGBWallet';
 import { NavigationRoutes } from 'src/navigation/NavigationRoutes';
-import InsufficiantBalancePopupContainer from 'src/screens/collectiblesCoins/components/InsufficiantBalancePopupContainer';
+import Relay from 'src/services/relay';
 import { Wallet } from 'src/services/wallets/interfaces/wallet';
+import { RealmSchema } from 'src/storage/enum';
+import dbManager from 'src/storage/realm/dbManager';
 import { AppTheme } from 'src/theme';
+import ModalContainer from 'src/components/ModalContainer';
+import AppText from 'src/components/AppText';
+import SwipeToAction from 'src/components/SwipeToAction';
+import { numberWithCommas } from 'src/utils/numberWithCommas';
+import { ApiHandler } from 'src/services/handler/apiHandler';
+import { TransactionKind } from 'src/services/wallets/enums';
+import Toast from 'src/components/Toast';
+
+type ServiceFeeProps = {
+  feeDetails: {
+    address: string;
+    fee: string;
+    includeTxFee: string;
+  };
+  onPay: () => void;
+};
+
+const ServiceFee = ({ feeDetails, onPay }: ServiceFeeProps) => {
+  const theme: AppTheme = useTheme();
+  const styles = getStyles(theme);
+
+  if (!feeDetails) return null;
+
+  return (
+    <View style={styles.containerFee}>
+      <View style={styles.amtContainer}>
+        <View style={styles.labelWrapper}>
+          <AppText style={styles.labelText}>{'Service Fee'}:</AppText>
+        </View>
+        <View style={styles.valueWrapper}>
+          <AppText style={styles.labelText}>{`${numberWithCommas(
+            feeDetails.fee,
+          )} sats`}</AppText>
+        </View>
+      </View>
+
+      <View style={styles.primaryCtaStyle}>
+        <SwipeToAction
+          title={'Swipe to Pay'}
+          loadingTitle={'Paying...'}
+          onSwipeComplete={onPay}
+        />
+      </View>
+    </View>
+  );
+};
 
 function AddAsset() {
   const navigation = useNavigation();
   const { issueAssetType } = useRoute().params;
   const wallet: Wallet = useWallets({}).wallets[0];
   const { translations } = useContext(LocalizationContext);
-  const { home, assets } = translations;
+  const { home } = translations;
   const theme: AppTheme = useTheme();
   const styles = getStyles(theme);
   const [visible, setVisible] = useState(false);
+  const rgbWallet: RGBWallet = dbManager.getObjectByIndex(RealmSchema.RgbWallet);
+  const [feeDetails, setFeeDetails] = useState(null);
+  const [showFeeModal, setShowFeeModal] = useState(false);
+
+  const unspent: RgbUnspent[] = rgbWallet.utxos.map(utxoStr => JSON.parse(utxoStr));
+  const colorable = unspent.filter(
+    utxo => utxo.utxo.colorable === true && utxo.rgbAllocations?.length === 0,
+  );
+
+  const getAssetIssuanceFeeMutation = useMutation(Relay.getAssetIssuanceFee);
+  const payServiceFeeFeeMutation = useMutation(ApiHandler.payServiceFee);
+
+  useEffect(() => {
+    if (getAssetIssuanceFeeMutation.isSuccess) {
+      const feeData = getAssetIssuanceFeeMutation.data;
+      if (feeData.fee > 0) {
+        setFeeDetails(feeData);
+        const feesPaid = wallet.specs.transactions.filter(
+          tx =>
+            tx.transactionKind === TransactionKind.SERVICE_FEE &&
+            tx.metadata?.assetId === '',
+        );
+        if (feesPaid.length > 0) {
+          navigation.replace(NavigationRoutes.ISSUESCREEN, {
+            issueAssetType,
+            addToRegistry: true,
+          });
+        } else {
+          setShowFeeModal(true);
+        }
+      } else {
+        navigation.replace(NavigationRoutes.ISSUESCREEN, {
+          issueAssetType,
+          addToRegistry: true,
+        });
+      }
+    } else if (getAssetIssuanceFeeMutation.error) {
+      Toast('Failed to fetch asset issuance fee.');
+      getAssetIssuanceFeeMutation.reset();
+    }
+  }, [getAssetIssuanceFeeMutation, navigation, issueAssetType, wallet.specs.transactions]);
+
+  useEffect(() => {
+    if (payServiceFeeFeeMutation.isSuccess) {
+      getAssetIssuanceFeeMutation.reset();
+      payServiceFeeFeeMutation.reset();
+      setShowFeeModal(false);
+      setTimeout(() => {
+        navigation.replace(NavigationRoutes.ISSUESCREEN, {
+          issueAssetType,
+          addToRegistry: true,
+        });
+      }, 700);
+    } else if (payServiceFeeFeeMutation.error) {
+      Toast(`Failed to pay service fee: ${payServiceFeeFeeMutation.error}`);
+      payServiceFeeFeeMutation.reset();
+      setShowFeeModal(false);
+    }
+  }, [payServiceFeeFeeMutation, navigation, issueAssetType]);
+
+  const canProceed = useMemo(() => {
+    return (
+      wallet?.specs.balances.confirmed + wallet?.specs.balances.unconfirmed >
+        0 || colorable.length > 0
+    );
+  }, [colorable.length, wallet?.specs.balances.confirmed, wallet?.specs.balances.unconfirmed]);
 
   return (
     <ScreenContainer>
       <AppHeader title={home.createAssets} subTitle={home.addAssetSubTitle} />
+
+      <View>
+        <ModalContainer
+          title={'Service Fee Information'}
+          subTitle={
+            'Please note that the service fee for this transaction is used exclusively for covering service charges on Tribe RGB. This fee will not be added to your reserve sats.'
+          }
+          height={Platform.OS === 'ios' ? '50%' : ''}
+          visible={showFeeModal}
+          enableCloseIcon={false}
+          onDismiss={() => {
+            setShowFeeModal(false);
+            getAssetIssuanceFeeMutation.reset();
+          }}
+        >
+          <ServiceFee
+            onPay={() => payServiceFeeFeeMutation.mutate({ feeDetails })}
+            feeDetails={feeDetails}
+          />
+        </ModalContainer>
+      </View>
+
       <View style={styles.container}>
         <SelectOption
           title={home.issueNew}
           backColor={theme.colors.inputBackground}
           style={styles.optionStyle}
           onPress={() => {
-            if (
-              wallet?.specs.balances.confirmed +
-                wallet?.specs.balances.unconfirmed ===
-              0
-            ) {
+            if (!canProceed) {
               setVisible(true);
             } else {
-              navigation.navigate(NavigationRoutes.ISSUESCREEN, {
-                issueAssetType: issueAssetType,
-              });
+              getAssetIssuanceFeeMutation.mutate();
             }
           }}
           testID="issue_new"
@@ -53,14 +184,10 @@ function AddAsset() {
           backColor={theme.colors.inputBackground}
           style={styles.optionStyle}
           onPress={() => {
-            if (
-              wallet?.specs.balances.confirmed +
-                wallet?.specs.balances.unconfirmed ===
-              0
-            ) {
+            if (!canProceed) {
               setVisible(true);
             } else {
-              navigation.navigate(NavigationRoutes.ENTERINVOICEDETAILS, {
+              navigation.replace(NavigationRoutes.ENTERINVOICEDETAILS, {
                 refresh: true,
               });
             }
@@ -68,27 +195,22 @@ function AddAsset() {
           testID="receive"
         />
       </View>
+
       <View>
         <ResponsePopupContainer
           visible={visible}
           enableClose={true}
           onDismiss={() => setVisible(false)}
           backColor={theme.colors.cardGradient1}
-          borderColor={theme.colors.borderColor}>
-          <InsufficiantBalancePopupContainer
-            primaryOnPress={() => {
-              setVisible(false);
-              setTimeout(() => {
-                navigation.replace(NavigationRoutes.RECEIVESCREEN);
-              }, 500);
-            }}
-            secondaryOnPress={() => setVisible(false)}
-          />
+          borderColor={theme.colors.borderColor}
+        >
+          <AppText>{'Insufficient Balance'}</AppText>
         </ResponsePopupContainer>
       </View>
     </ScreenContainer>
   );
 }
+
 const getStyles = (theme: AppTheme) =>
   StyleSheet.create({
     container: {
@@ -98,5 +220,35 @@ const getStyles = (theme: AppTheme) =>
       marginVertical: hp(5),
       paddingHorizontal: 20,
     },
+    containerFee: {
+      borderTopColor: theme.colors.borderColor,
+      borderTopWidth: 1,
+      paddingTop: hp(15),
+    },
+    labelWrapper: {
+      width: '45%',
+    },
+    valueWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      width: '55%',
+    },
+    labelText: {
+      color: theme.colors.headingColor,
+    },
+    primaryCtaStyle: {
+      marginTop: hp(30),
+    },
+    amtContainer: {
+      marginVertical: hp(20),
+      padding: hp(15),
+      borderRadius: 15,
+      alignItems: 'center',
+      borderColor: '#787878',
+      borderWidth: 1,
+      borderStyle: 'dashed',
+      flexDirection: 'row',
+    },
   });
+
 export default AddAsset;
