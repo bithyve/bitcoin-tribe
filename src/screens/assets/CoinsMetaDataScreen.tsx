15a461b2e553acea483aa88e130d71a696eb7b8f
akshay8551 <akshayc@bithyve.com>
2025-02-06 15:21:36 +0530

    Coin & Collectibles Meta Data - update labels

--- src/screens/assets/CoinsMetaDataScreen.tsx
+++ src/screens/assets/CoinsMetaDataScreen.tsx
@@ -46,7 +46,7 @@ const CoinsMetaDataScreen = () => {
   const theme: AppTheme = useTheme();
   const styles = React.useMemo(() => getStyles(theme), [theme]);
   const { translations } = useContext(LocalizationContext);
-  const { assets } = translations;
+  const { assets, home } = translations;
   const { assetId } = useRoute().params;
   const coin = useObject<Coin>(RealmSchema.Coin, assetId);
   const { mutate, isLoading } = useMutation(ApiHandler.getAssetMetaData);
@@ -71,9 +71,9 @@ const CoinsMetaDataScreen = () => {
           style={styles.scrollingContainer}
           showsVerticalScrollIndicator={false}>
           <View style={styles.rowWrapper}>
-            <Item title={assets.name} value={coin.name} width={'45%'} />
+            <Item title={home.assetName} value={coin.name} width={'45%'} />
             <Item
-              title={assets.ticker}
+              title={home.assetTicker}
               value={coin.metaData && coin.metaData.ticker}
               width={'45%'}
             />
@@ -94,7 +94,9 @@ const CoinsMetaDataScreen = () => {
           <View style={styles.rowWrapper}>
             <Item
               title={assets.issuedSupply}
-              value={coin.metaData && numberWithCommas(coin.metaData.issuedSupply)}
+              value={
+                coin.metaData && numberWithCommas(coin.metaData.issuedSupply)
+              }
               width={'45%'}
             />
             <Item
