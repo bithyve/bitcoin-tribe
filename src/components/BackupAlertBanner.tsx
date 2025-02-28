143c989ed30a35a18a390e29e7f99d1d73d2fc6e
akshay8551 <akshayc@bithyve.com>
2025-02-28 13:51:24 +0530

    added condition

--- src/components/BackupAlertBanner.tsx
+++ src/components/BackupAlertBanner.tsx
@@ -19,7 +19,7 @@ const BackupAlertBanner = () => {
   const theme: AppTheme = useTheme();
   const styles = getStyles(theme, hasNotch);
   const [visible, setVisible] = useState(false);
-  return true ? (
+  return isBackupInProgress ? (
     <>
       <AppTouchable style={styles.banner} onPress={() => setVisible(true)}>
         <View style={styles.titleWrapper}>
