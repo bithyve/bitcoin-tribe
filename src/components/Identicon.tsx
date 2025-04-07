5209d4447f5a963fbaecf76535697819007cfeb3
akshay8551 <akshayc@bithyve.com>
2025-04-07 08:56:48 +0530

    Identicon - handle Canvas ref

--- src/components/Identicon.tsx
+++ src/components/Identicon.tsx
@@ -4,23 +4,42 @@ import Canvas from 'react-native-canvas';
 import { IndenticonLib } from './Identiconlib';
 
 interface Props {
-    value: string;
-    style?: {[key: string]: any};
-    size?: number;
+  value: string;
+  style?: { [key: string]: any };
+  size?: number;
 }
 
 const Identicon = ({ value, style, size = 45 }: Props) => {
-    const handleCanvas = useRef(null);
+  const canvasRef = useRef(null);
 
-    useEffect(() => {
-        if (handleCanvas.current) {IndenticonLib.generateSync(handleCanvas.current, { id: value, size }, size);}
-    }, []);
+  const handleCanvas = (canvas: any) => {
+    if (canvas) {
+      canvas.width = size;
+      canvas.height = size;
 
-    return (
-        <View style={[{width: size, height: size, borderRadius: 30, overflow: 'hidden'}, style]}>
-            <Canvas ref={handleCanvas}/>
-        </View>
-    );
+      IndenticonLib.generateSync(canvas, { id: value, size }, size);
+    }
+  };
+
+  useEffect(() => {
+    if (handleCanvas.current) {
+      IndenticonLib.generateSync(
+        handleCanvas.current,
+        { id: value, size },
+        size,
+      );
+    }
+  }, []);
+
+  return (
+    <View
+      style={[
+        { width: size, height: size, borderRadius: 30, overflow: 'hidden' },
+        style,
+      ]}>
+      <Canvas ref={handleCanvas} />
+    </View>
+  );
 };
 
 export default Identicon;
