419be5e69d4fb9b10e9fe9b993f3aa5583a6bcad
akshay8551 <akshayc@bithyve.com>
2025-04-18 13:19:08 +0530

    Identicon - show indenticon when coin image not available and  added jdenticon lib #1042

--- src/components/Identicon.tsx
+++ src/components/Identicon.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import Canvas from 'react-native-canvas';
-import { IndenticonLib } from './Identiconlib';
+import Svg, { G, Path } from 'react-native-svg';
+import { generateIdenticonSvg } from 'src/utils/identicon';
+import { SvgXml } from 'react-native-svg';
 
 interface Props {
   value: string;
@@ -10,43 +11,20 @@ interface Props {
 }
 
 const Identicon = ({ value, style, size = 45 }: Props) => {
-  const canvasRef = useRef(null);
-
-  const handleCanvas = useCallback(
-    (canvas: any) => {
-      if (canvas) {
-        canvas.width = size;
-        canvas.height = size;
-
-        const ctx = canvas.getContext('2d');
-
-        if (ctx) {
-          IndenticonLib.generateSync(canvas, { id: value, size }, size);
-        } else {
-          console.warn('Canvas context is not available.');
-        }
-      }
-    },
-    [value, size],
-  );
-
-  useEffect(() => {
-    if (handleCanvas.current) {
-      IndenticonLib.generateSync(
-        handleCanvas.current,
-        { id: value, size },
-        size,
-      );
-    }
-  }, []);
+  const svgMarkup = generateIdenticonSvg(value, size);
 
   return (
     <View
       style={[
-        { width: size, height: size, borderRadius: 30, overflow: 'hidden' },
+        {
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+          overflow: 'hidden',
+        },
         style,
       ]}>
-      <Canvas ref={handleCanvas} />
+      <SvgXml xml={svgMarkup} width={size} height={size} />
     </View>
   );
 };
