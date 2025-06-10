a86672d5e59437182f81aa1c53427028bfbe8b9a
antuz123 <43998049+antuz123@users.noreply.github.com>
2025-06-10 16:51:00 +0530

    feat: make setup script configurable

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # enabling node core modules
 # adding node core modules support in react-native
 rn-nodeify --install buffer,events,process,stream,inherits,path,assert,crypto,constants --hack --yarn
@@ -6,4 +8,17 @@ rn-nodeify --install buffer,events,process,stream,inherits,path,assert,crypto,co
 cd ios && RCT_NEW_ARCH_ENABLED=0 pod install
 
 # android SDK location configuration
-cd ../android && touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" >local.properties
+cd ../android
+
+# Determine Android SDK path. Prefer an argument, then ANDROID_HOME, then the
+# default macOS location.
+if [ -n "$1" ]; then
+  SDK_PATH="$1"
+elif [ -n "$ANDROID_HOME" ]; then
+  SDK_PATH="$ANDROID_HOME"
+else
+  SDK_PATH="/Users/$(whoami)/Library/Android/sdk"
+fi
+
+touch local.properties
+echo "sdk.dir = $SDK_PATH" > local.properties
