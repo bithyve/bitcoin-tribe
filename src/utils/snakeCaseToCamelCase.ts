27603a2188675735d193c63f53e641b23e7af540
antuz123 <43998049+antuz123@users.noreply.github.com>
2025-06-10 16:50:54 +0530

    rename snakeCase util and update imports

--- src/utils/snakeCaseToCamelCase.ts
+++ src/utils/snakeCaseToCamelCase.ts
@@ -2,13 +2,13 @@ function toCamelCase(str: string): string {
   return str.replace(/_([a-z])/g, (match, letter) => letter.toUpperCase());
 }
 
-export function snakeCaseToCamelCaseCase(obj: any): any {
+export function snakeCaseToCamelCase(obj: any): any {
   if (Array.isArray(obj)) {
-    return obj.map(v => snakeCaseToCamelCaseCase(v));
+    return obj.map(v => snakeCaseToCamelCase(v));
   } else if (obj !== null && obj.constructor === Object) {
     return Object.keys(obj).reduce((result: any, key: string) => {
       const camelCaseKey = toCamelCase(key);
-      result[camelCaseKey] = snakeCaseToCamelCaseCase(obj[key]);
+      result[camelCaseKey] = snakeCaseToCamelCase(obj[key]);
       return result;
     }, {});
   }
