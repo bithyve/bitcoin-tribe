8ab92fbecdbc7668d38cf7ce159e6c185ab2ee3e
Shashank <shashank@cakesofttech.com>
2025-06-30 08:51:31 +0530

    update version check to support version 153 and above

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -134,7 +134,7 @@ function HomeScreen() {
   }, [navigation]);
 
   useEffect(() => {
-    if (Number(versionNumber) < 138) {
+    if (Number(versionNumber) < 153) {
       Alert.alert(
         'Unsupported Version',
         'This version of Tribe is no longer supported. Please setup a new wallet to continue.',
