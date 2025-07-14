622aac56bdeaef71e1923b2ed1bb93e04a12878c
Shashank <shashank@cakesofttech.com>
2025-07-15 01:20:36 +0530

    refactor precision handling

--- src/screens/assets/CoinsMetaDataScreen.tsx
+++ src/screens/assets/CoinsMetaDataScreen.tsx
@@ -246,9 +246,7 @@ const CoinsMetaDataScreen = () => {
               value={
                 coin.metaData && coin.metaData.precision === 0
                 ? numberWithCommas(Number(coin?.metaData?.issuedSupply))
-                : numberWithCommas(Number(coin?.metaData?.issuedSupply) / 10 ** coin?.metaData?.precision) +
-                  '.' +
-                  '0'.repeat(coin?.metaData?.precision)
+                : numberWithCommas(Number(coin?.metaData?.issuedSupply) / 10 ** coin?.metaData?.precision)
               }
               width={'45%'}
             />
