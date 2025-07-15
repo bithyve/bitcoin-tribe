7b6051d51b7c73c313826a8960b1d4499a4accbc
akshay8551 <akshayc@bithyve.com>
2025-07-15 14:43:50 +0530

    increased schemaVersion

--- src/storage/realm/realm.ts
+++ src/storage/realm/realm.ts
@@ -8,7 +8,7 @@ export class RealmDatabase {
 
   public static file = 'tribe.realm';
 
-  public static schemaVersion = 69;
+  public static schemaVersion = 70;
 
   /**
    * initializes/opens realm w/ appropriate configuration
