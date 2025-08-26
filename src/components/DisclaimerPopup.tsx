4d5faa7c6eee65ae8985c50f72c05b723b5f764c
Shashank <shashank@cakesofttech.com>
2025-08-26 15:19:03 +0530

    refactor: enhance DisclaimerPopup with interactive checkbox for terms acceptance

--- src/components/DisclaimerPopup.tsx
+++ src/components/DisclaimerPopup.tsx
@@ -1,10 +1,8 @@
 import { Dimensions, StyleSheet, View } from 'react-native';
-import React, { useContext } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTheme } from 'react-native-paper';
 import { useMMKVBoolean } from 'react-native-mmkv';
 import RenderHTML from 'react-native-render-html';
-
-import { LocalizationContext } from 'src/contexts/LocalizationContext';
 import { AppTheme } from 'src/theme';
 import { Keys } from 'src/storage';
 import AppText from './AppText';
@@ -12,7 +10,10 @@ import Buttons from './Buttons';
 import { hp } from 'src/constants/responsive';
 import ResponsePopupContainer from './ResponsePopupContainer';
 import CheckIcon from 'src/assets/images/checkIcon.svg';
+import UncheckIcon from 'src/assets/images/uncheckIcon.svg';
+import UncheckIconLight from 'src/assets/images/unCheckIcon_light.svg';
 import CheckIconLight from 'src/assets/images/checkIcon_light.svg';
+import AppTouchable from './AppTouchable';
 
 interface Props {
   visible: boolean;
@@ -32,8 +33,14 @@ const DisclaimerPopup: React.FC<Props> = ({
   const theme: AppTheme = useTheme();
   const [isThemeDark] = useMMKVBoolean(Keys.THEME_MODE);
   const styles = getStyles(theme);
-  const { translations } = useContext(LocalizationContext);
-  const { common, settings } = translations;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkIcon = useMemo(() => {
+    if (isThemeDark) {
+      return isChecked ? <CheckIcon /> : <UncheckIcon />;
+    }
+    return isChecked ? <CheckIconLight /> : <UncheckIconLight />;
+  }, [isChecked]);
 
   return (
     <ResponsePopupContainer
@@ -49,9 +56,11 @@ const DisclaimerPopup: React.FC<Props> = ({
           />
         </View>
         <View style={styles.checkIconContainer}>
-          <View style={styles.checkIconWrapper}>
-            {isThemeDark ? <CheckIcon /> : <CheckIconLight />}
-          </View>
+          <AppTouchable
+            onPress={() => setIsChecked(!isChecked)}
+            style={styles.checkIconWrapper}>
+            {checkIcon}
+          </AppTouchable>
           <AppText variant="body2" style={styles.infoText}>
             {'By continuing, you accept these terms'}
           </AppText>
@@ -60,6 +69,7 @@ const DisclaimerPopup: React.FC<Props> = ({
         <Buttons
           primaryTitle={primaryCtaTitle}
           primaryOnPress={primaryOnPress}
+          disabled={!isChecked}
           width={'100%'}
           height={hp(14)}
         />
