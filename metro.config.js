901bbe80be8f999f09f55bc32684da59361c9c15
Parsh <parsh.cosmos11@gmail.com>
2025-10-11 17:38:38 +0200

    adds: sodium-native and sodium-universal polyfill updates: bare-pack script

--- metro.config.js
+++ metro.config.js
@@ -19,6 +19,20 @@ const config = {
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...sourceExts, 'svg'],
+    resolveRequest: (context, moduleName, platform) => {
+      // Redirect both sodium-native and sodium-universal to sodium-javascript for React Native
+      // sodium-universal uses the browser field which Metro doesn't respect by default
+      if (moduleName === 'sodium-native' || moduleName === 'sodium-universal') {
+        return {
+          filePath: require.resolve('sodium-javascript'),
+          type: 'sourceFile',
+        };
+      }
+
+      // Use default resolver for everything else
+      return context.resolveRequest(context, moduleName, platform);
+    },
+
   },
 };
 
